185090a99a7665e95e8b692e5318be2e1962b6ed
Changyuan Lin <coreballium@outlook.com>
2023-05-01 04:56:25 -0400

    Go back to using JSON instead of CSV

--- scripts/extract-timestamp.js
+++ scripts/extract-timestamp.js
@@ -7,7 +7,6 @@ const __filename = url.fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const imageDir = path.join(__dirname, '..', 'public', 'images')
 
-const header = ['filename', 'timestamp'].join('\t')
 const filenames = await fs.readdir(imageDir)
 const data = await Promise.all(
   filenames.map(async (filename) => {
@@ -15,10 +14,11 @@ const data = await Promise.all(
     const [date, time] = tags['DateTimeOriginal'].description.split(' ')
     const offset = tags['OffsetTimeOriginal'].description
     const timestamp = date.replaceAll(':', '-') + 'T' + time + offset
-    return [filename, timestamp].join('\t')
+    return {
+      filename,
+      timestamp,
+    }
   })
 )
 
-const output = [header, ...data].join('\n')
-
-await fs.writeFile('data.csv', output)
+await fs.writeFile('data.json', JSON.stringify(data, null, 2))
